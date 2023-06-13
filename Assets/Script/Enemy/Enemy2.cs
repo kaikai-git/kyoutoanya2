cbfb26e1a423b683c27721507fce3f0cb620249c
kaikai-git <kaiandika@icloud.com>
2023-06-13 10:49:01 +0900

    upsate
    てきがみえない

---
+++ Assets/Script/Enemy/Enemy2.cs
@@ -0,0 +1,32 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    private Color color;
+
+    void Start()
+    {
+        color = gameObject.GetComponent<SpriteRenderer>().color;
+
+        color.a = 0.0f;
+        gameObject.GetComponent<SpriteRenderer>().color = color;
+
+        //•Ê‚Ì•û–@
+        //gameObject.GetComponent<SpriteRenderer>().color = new Color(0.8f, 0.3f, 0.1f, 0.5f);
+    }
+
+    void OnTriggerStay(Collider  other)
+    {
+        if(other.gameObject.tag == "CanSee")
+        {
+            color.a = 1.0f;
+            gameObject.GetComponent<SpriteRenderer>().color = color;
+            Debug.Log("hiy");
+        }
+        
+
+    }
+
+
+}
