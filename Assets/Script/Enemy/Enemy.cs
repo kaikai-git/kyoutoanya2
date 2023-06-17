83687bf186c085703b4afe3df0a84b255e07e1aa
kaikai-git <kaiandika@icloud.com>
2023-06-17 18:32:00 +0900

    SpecialPerformanace

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
 public class Enemy : MonoBehaviour
 {
 
     public GameObject TargetObject;
-
+    public RectTransform SpecialPerform;
     UnityEngine.AI.NavMeshAgent navMeshAgent;
-
+    private bool peformtime = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +21,26 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         // NavMesh
         if (navMeshAgent.pathStatus != UnityEngine.AI.NavMeshPathStatus.PathInvalid)
         {
             // NavMeshAgent�ɖړI�n���Z�b�g
             navMeshAgent.SetDestination(TargetObject.transform.position);
+        }
+
+        if(peformtime)
+        {
+            Vector3 currentPosition = SpecialPerform.anchoredPosition3D;
+            Debug.Log("2");
+            if (currentPosition.x < -1)
+            {
+                Vector3 newPosition = currentPosition + new Vector3(10f, 0f, 0f);
+                SpecialPerform.anchoredPosition3D = newPosition;
+                Debug.Log("d");
+            }
+           
+            //if(newPosition = )
         }
     }
 
@@ -31,8 +49,21 @@ public class Enemy : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             Debug.Log("ge-muo-ba-");
-            SceneManager.LoadScene("GameOver");
+            //SceneManager.LoadScene("GameOver");
+        }
+
+        //SpecialPerformance
+        else if (other.CompareTag("PlayerSub"))
+        {
+            
+                peformtime = true;
+            
         }
         
     }
+
+   
+   
+    
+
 }
